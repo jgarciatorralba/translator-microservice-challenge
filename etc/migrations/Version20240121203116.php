6fb29bbe32689eaa1106f1e0cf0fb9340c55ce28
Jorge García <jgarciatorralba@gmail.com>
2024-01-21 21:34:23 +0100

    update migration

--- etc/migrations/Version20240121203116.php
+++ etc/migrations/Version20240121203116.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240121200057 extends AbstractMigration
+final class Version20240121203116 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,9 @@ final class Version20240121200057 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE translations (id UUID NOT NULL, source_lang VARCHAR(8) NOT NULL, original_text VARCHAR(255) NOT NULL, target_lang VARCHAR(8) NOT NULL, status text DEFAULT \'queued\' NOT NULL, translated_text VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE translations (id UUID NOT NULL, source_lang text NOT NULL, original_text VARCHAR(255) NOT NULL, target_lang text NOT NULL, status text DEFAULT \'queued\' NOT NULL, translated_text VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN translations.source_lang IS \'(DC2Type:supported_language)\'');
+        $this->addSql('COMMENT ON COLUMN translations.target_lang IS \'(DC2Type:supported_language)\'');
         $this->addSql('COMMENT ON COLUMN translations.status IS \'(DC2Type:processing_status)\'');
         $this->addSql('COMMENT ON COLUMN translations.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('COMMENT ON COLUMN translations.updated_at IS \'(DC2Type:datetime_immutable)\'');
