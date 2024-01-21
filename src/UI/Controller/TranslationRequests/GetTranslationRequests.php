4d82a68f8fa2ac0098bd6d5abde1cd95d93b13a3
Jorge García <jgarciatorralba@gmail.com>
2024-01-21 12:54:49 +0100

    WIP first route: get translation requests

---
+++ src/UI/Controller/TranslationRequests/GetTranslationRequests.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Controller\Carts;
+
+use App\TranslationRequests\Application\Query\GetTranslationRequests\GetTranslationRequestsQuery;
+use App\UI\Controller\BaseController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+final class GetTranslationRequestsController extends BaseController
+{
+    public function __invoke(): Response
+    {
+        $response = $this->ask(new GetTranslationRequestsQuery());
+        return new JsonResponse($response->data(), Response::HTTP_OK);
+    }
+}
