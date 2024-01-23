bef2a70024c1dd379608e023909c576f44ab1efa
Jorge García <jgarciatorralba@gmail.com>
2024-01-23 22:43:54 +0100

    add service "UpdateTranslation"

---
+++ src/Translations/Domain/Service/UpdateTranslation.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Translations\Domain\Service;
+
+use App\Translations\Domain\Contract\TranslationRepository;
+use App\Translations\Domain\Translation;
+use App\Translations\Domain\ValueObject\StatusEnum;
+use DateTimeImmutable;
+
+final class UpdateTranslation
+{
+    public function __construct(
+        private readonly TranslationRepository $translationRepository
+    ) {
+    }
+
+    /**
+     * @param array <string, string|StatusEnum|DateTimeImmutable> $updatedData
+     */
+    public function __invoke(Translation $translation, array $updatedData): void
+    {
+        $hasChanged = false;
+
+        if (!empty($updatedData['status']) && $updatedData['status'] !== $translation->status()) {
+            $translation->updateStatus($updatedData['status']);
+            $hasChanged = true;
+        }
+        if (
+            !empty($updatedData['translatedText'])
+            && $updatedData['translatedText'] !== $translation->translatedText()
+        ) {
+            $translation->updateTranslatedText($updatedData['translatedText']);
+            $hasChanged = true;
+        }
+
+        if ($hasChanged) {
+            $translation->updateUpdatedAt($updatedData['updatedAt']);
+            $this->translationRepository->update($translation);
+        }
+    }
+}
