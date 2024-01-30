e0d511b03439093d191d90a79fb5214f163a1bd5
Jorge Garcia <jorge@adkomo.com>
2024-01-30 10:27:10 +0100

    add missing mapping for MissingProviderException

--- src/UI/Subscriber/ExceptionHttpStatusCodeMapper.php
+++ src/UI/Subscriber/ExceptionHttpStatusCodeMapper.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace App\UI\Subscriber;
 
+use App\Translations\Domain\Exception\MissingProviderException;
 use App\Translations\Domain\Exception\TranslationNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 
 final class ExceptionHttpStatusCodeMapper
 {
     private const EXCEPTIONS = [
-        TranslationNotFoundException::class => Response::HTTP_NOT_FOUND
+        TranslationNotFoundException::class => Response::HTTP_NOT_FOUND,
+        MissingProviderException::class => Response::HTTP_INTERNAL_SERVER_ERROR
     ];
 
     public function getStatusCodeFor(string $exceptionClass): ?int
