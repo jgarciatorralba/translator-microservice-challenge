dc45fce4843282b8a9066f79cfa6384793260757
Jorge García <jgarciatorralba@gmail.com>
2024-02-25 20:52:12 +0100

    re-work DoctrineCriteriaConverter

--- src/Shared/Infrastructure/Persistence/Doctrine/DoctrineCriteriaConverter.php
+++ src/Shared/Infrastructure/Persistence/Doctrine/DoctrineCriteriaConverter.php
@@ -6,109 +6,71 @@ namespace App\Shared\Infrastructure\Persistence\Doctrine;
 
 use App\Shared\Domain\Criteria\Criteria;
 use App\Shared\Domain\Criteria\Filter\CompositeFilter;
-use App\Shared\Domain\Criteria\Filter\FilterConditionEnum;
-use App\Shared\Domain\Criteria\Filter\FilterOperatorEnum;
 use App\Shared\Domain\Criteria\Filter\SimpleFilter;
 use App\Shared\Domain\Criteria\Order\Order;
 use Doctrine\Common\Collections\Criteria as DoctrineCriteria;
 use Doctrine\Common\Collections\Expr\Comparison;
 use Doctrine\Common\Collections\Expr\CompositeExpression;
 use Doctrine\Common\Collections\Expr\Expression;
-use Doctrine\Common\Collections\ExpressionBuilder;
 
 final class DoctrineCriteriaConverter
 {
     public function __construct(
-        private readonly Criteria $criteria,
-        private readonly ExpressionBuilder $eb
+        private readonly Criteria $criteria
     ) {
     }
 
     public static function convert(Criteria $criteria): DoctrineCriteria
     {
-        $converter = new self($criteria, DoctrineCriteria::expr());
+        $converter = new self($criteria);
 
         return $converter->convertToDoctrineCriteria();
     }
 
     private function convertToDoctrineCriteria(): DoctrineCriteria
     {
-        $doctrineCriteria = new DoctrineCriteria();
-
-        $this->buildFilterExpressions($doctrineCriteria, $this->criteria);
-        $doctrineCriteria->orderBy($this->formatOrder($this->criteria));
-        $doctrineCriteria->setFirstResult($this->criteria->offset() ?? 0);
-        $doctrineCriteria->setMaxResults($this->criteria->limit());
-
-        return $doctrineCriteria;
+        return new DoctrineCriteria(
+            $this->buildExpression($this->criteria),
+            $this->formatOrder($this->criteria),
+            $this->criteria->offset() ?? 0,
+            $this->criteria->limit()
+        );
     }
 
-    private function buildFilterExpressions(
-        DoctrineCriteria &$doctrineCriteria,
-        Criteria $criteria
-    ): void {
-        if (!$criteria->hasFilters()) {
-            return;
-        }
-
-        /** @param SimpleFilter|CompositeFilter $filter */
-        foreach ($criteria->filters()->plainFilters() as $filter) {
-            $expression = $this->buildExpression($filter);
-            if ($expression) {
-                if ($criteria->filters()->condition() === FilterConditionEnum::AND) {
-                    $doctrineCriteria->andWhere($expression);
-                } elseif ($criteria->filters()->condition() === FilterConditionEnum::OR) {
-                    $doctrineCriteria->orWhere($expression);
-                }
-            }
-        }
-    }
-
-    private function buildExpression(SimpleFilter|CompositeFilter $filter): ?Expression
+    private function buildExpression(Criteria $criteria): ?CompositeExpression
     {
-        if ($filter instanceof SimpleFilter) {
-            return $this->buildComparison($filter);
-        } elseif ($filter instanceof CompositeFilter) {
-            $comparisons = [];
-            /** @var SimpleFilter $simpleFilter */
-            foreach ($filter->filters() as $simpleFilter) {
-                $comparison = $this->buildComparison($simpleFilter);
-                if (!empty($comparison)) {
-                    $comparisons[] = $comparison;
-                }
-            }
-
-            return $this->buildCompositeExpression($comparisons, $filter->condition());
+        if ($criteria->hasFilters()) {
+            return new CompositeExpression(
+                $criteria->filters()->condition()->value,
+                array_map(
+                    $this->buildComparison(),
+                    $criteria->filters()->plainFilters()
+                )
+            );
         }
+
+        return null;
     }
 
-    private function buildComparison(SimpleFilter $filter): ?Comparison
+    private function buildComparison(): callable
     {
-        switch ($filter->operator()) {
-            case FilterOperatorEnum::EQUAL:
-                return $this->eb->eq($filter->field(), $filter->value());
-            case FilterOperatorEnum::LOWER_THAN:
-                return $this->eb->lt($filter->field(), $filter->value());
-            default:
-                return null;
-        }
-    }
-
-    /** @param Comparison[] $comparisons */
-    private function buildCompositeExpression(
-        array $comparisons,
-        FilterConditionEnum $condition
-    ): ?CompositeExpression {
-        if (empty($comparisons)) {
-            return null;
-        }
-
-        switch ($condition) {
-            case FilterConditionEnum::AND:
-                return $this->eb->andX(...$comparisons);
-            case FilterConditionEnum::OR:
-                return $this->eb->orX(...$comparisons);
-        }
+        return function (SimpleFilter|CompositeFilter $filter): Expression {
+            if ($filter instanceof SimpleFilter) {
+                return new Comparison(
+                    $filter->field(),
+                    $filter->operator()->value,
+                    $filter->value()
+                );
+            } else {
+                return new CompositeExpression(
+                    $filter->condition()->value,
+                    array_map(
+                        $this->buildComparison(),
+                        $filter->filters()
+                    )
+                );
+            }
+        };
     }
 
     /** @return array <string, string> */
